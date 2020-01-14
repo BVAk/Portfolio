0e7ece4cf4573f97c9ffbef56c66f454a10ad28c
BVAk <bekkviktoria55@gmail.com>
2020-01-14 14:24:34 +0200

    trying gallery

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -6,8 +6,8 @@
           <div class="flex">
 
             <h1>Projects</h1>
-
-            <gallery :images="images" :index="index" @close="index = null"></gallery>
+            <div class="row justify-content">
+    <gallery :images="images" :index="index" @close="index = null"></gallery>
     <div
       class="image"
       v-for="(image, imageIndex) in images"
@@ -15,62 +15,83 @@
       @click="index = imageIndex"
       :style="{ backgroundImage: 'url(' + image + ')', width: '300px', height: '200px' }"
     ></div>
-          </div>
-        </section>
+    </div>
+  </div>
+</section>
+</div>
+</div>
+    <div class="row justify-content">
+      <div class="col">
+        <h1>JavaLearn</h1>
+        <lingallery  :items="items" />
+        <p>The project is designed for distance learning. The site is written in PHP - Laravel framework,</p>
+      </div>
+      <div class="col">
+        <lingallery  :items="items" />
       </div>
     </div>
   </div>
 </template>
 <script>
-   import VueGallery from 'vue-gallery';
+  import Lingallery from 'lingallery';
+  import VueGallery from 'vue-gallery';
   export default {
-    name: "Projects",
-    data: function () {
+    data() {
       return {
+        items: [{
+          src: 'javalearn.png',
+          thumbnail: 'javalearn.png',
+          caption: "JavaLearn"
+        },
+        {
+          src: 'https://picsum.photos/600/400/?image=10',
+          thumbnail: 'https://picsum.photos/64/64/?image=10'
+        }
+        ],
         images: [
-         {title:'JavaLearn', description:'Java descropt', href: 'javalearn.png'},
-          {title:'JavaLearn', description:'Notdescropt', href:'https://dummyimage.com/1600/ffffff/000000'},
+         'javalearn.png',
+          'https://dummyimage.com/1600/ffffff/000000',
           'https://dummyimage.com/1280/000000/ffffff',
           'https://dummyimage.com/400/000000/ffffff',
         ],
         index: null
+     
       };
     },
- 
     components: {
+      Lingallery,
+      
       'gallery': VueGallery
-    },
-  };
 
+    }
+  }
 </script>
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
   /* Base */
-    .image {
+
+  #projectid {
+    height: -webkit-fill-available;
+    text-align: center;
+
+    margin: 15px;
+    justify-content: center;
+
+    
+  }
+  .image {
     float: left;
     background-size: cover;
     background-repeat: no-repeat;
     background-position: center center;
-    border: 1px solid #ebebeb;
+    border: 1px solid #ddc3c3;
     margin: 5px;
-  }
-  #projectid {
-    height: -webkit-fill-available;
-    text-align: center;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    
-    overflow: auto;
-  }
+  }  
 
- #projectid h1 {
-    margin: 0;
-    
-  }
-  .flex{ font-size: 32pt;
-    line-height: 36pt;    
+  .flex {
+    font-size: 32pt;
+    line-height: 36pt;
     font-family: "Raleway";
-    
+
   }
 </style>
