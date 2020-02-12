c1dda498327af3208c020b6e834245825a3ba9e8
BVAk <bekkviktoria55@gmail.com>
2020-02-12 22:07:43 +0200

    add about

---
+++ src/components/About.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="about" >
+   <div class="overflow-wrap">
+
+      <div class="modal-wrap flex">
+        <section class="flex height-fix">
+          <div class="flex">
+            <h1>About</h1>
+          </div>
+        </section>
+      </div>
+    </div>
+     
+    <div class="row justify-content">
+      <div class="col">
+       
+        <img width="100%" src="me.JPG">
+      </div>
+      <div class="col">
+        
+      </div>
+    </div>
+  </div>
+  
+</template>
+
+<script>
+    export default {
+        name: "About"
+
+    };
+</script>
