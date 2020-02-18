f8c49604748312fb10f3e5b3d9ba85c863cf43a6
BVAk <bekkviktoria55@gmail.com>
2020-02-18 23:12:20 +0200

    change about mail

--- src/components/About.vue
+++ src/components/About.vue
@@ -9,35 +9,43 @@
         </section>
       </div>
     </div>
-    <div id="CV" class="p-3 m-3">
-      <div class="row">
-      <div class="header">
-      <h2>Viktoriia Bekk</h2>
-      <h3>PHP Developer</h3>
+    <div id="CV">
       <div class="row">
         <div class="col">
-          <div class="row"> <font-awesome-icon :icon="['fas','reply-all']" width="20px" /><a href="mailto:bekkviktoria55@gmail.com">bekkviktoria55@gmail.com</a></div>
-      <div class="row"><font-awesome-icon :icon="['fab', 'bitbucket']" width="20px" /><a href="https://bitbucket.org/VikaBekk/">
-        https://bitbucket.org/VikaBekk</a></div>
-
-        <div class="row"><font-awesome-icon :icon="['fab', 'github']" width="20px" /><a href="https://github.com/BVAk">
-            https://github.com/BVAk</a></div>
-       
-    </div>
-      <div class="col">
-       <font-awesome-icon icon="mobile-alt" width="20px" /> <a href="tel:+380956289382">+380956289382</a>
-        <label></label>
-        <label></label>
+        <div class="header" >
+          
+              <h2>Viktoriia Bekk</h2>
+          <h3>PHP Developer</h3>
+          <div class="row">
+            
+            <div id="contact">
+              <div class="col">
+                <div class="row"> <font-awesome-icon :icon="['fab','linkedin']" width="20px" /><a href="https://www.linkedin.com/in/viktoriabekk-a79595147/">https://www.linkedin.com/in/viktoriabekk</a>
+                </div>
+                <div class="row"><font-awesome-icon :icon="['fab', 'bitbucket']" width="20px" /><a href="https://bitbucket.org/VikaBekk/">https://bitbucket.org/VikaBekk</a>
+                </div>
+                <div class="row"><font-awesome-icon :icon="['fab', 'github']" width="20px" /><a href="https://github.com/BVAk">https://github.com/BVAk</a>
+                </div>
+              </div>
+              <div class="col">
+                <div class="row">  <font-awesome-icon :icon="['fas', 'mobile-alt']" width="20px" /> <a href="tel:+380956289382">+380956289382</a>
+                </div>
+                <div class="row"> <font-awesome-icon :icon="['far','envelope']" width="20px" /><a href="mailto:bekkviktoria55@gmail.com">bekkviktoria55@gmail.com</a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        </div>
+          <div class="col">
+            <div class="img">
+              <img src="me.jpg" width="200px">
+            </div>
+          </div>
+        </div>
       </div>
-    </div>
       </div>
-      <div class="img">
-        <img src="me.jpg" width="200px">
-      </div>
-    </div>
-  </div>
-  
-  </div>
+   
   
 </template>
 
@@ -65,16 +73,30 @@
     
     #CV {
         text-align: center;
+        margin: 2vw 10vw;
     }
     
-    .header {
+    #header {
         display: flex;
-        flex-direction: column;
+        flex-direction: row;
         justify-content: center;
-        width: calc(100% - 300px);
+        width: calc(100% - 200px);
     }
     
-    .img {
+    #contact {
+        text-align: left;
+        padding-left: 0;
+        display: flex;
+        justify-content: flex-start;
+        align-items: flex-start;
+        flex-wrap: wrap;
+    }
+    
+    #contact a {
+        margin: 0.5rem;
+    }
+    
+    #img {
         direction: ltr;
         align-self: flex-start;
         position: relative;
